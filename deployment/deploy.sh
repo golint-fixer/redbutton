7f8e35add447ebc30b9c6565a1bb4a8cbc8ac482
Viktoras Makauskas <viktoras@neglostyti.com>
2016-01-29 17:51:57 +0200

    creating new instances before cleaning up old; still need to attach to load balancer

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,8 +1,11 @@
 ansible-playbook playbooks/build.yml
-ansible-playbook playbooks/decomission.yml
 
 inventory/ec2.py --refresh-cache > /dev/null
+ansible-playbook playbooks/tag_for_decomission.yml
+
 ansible-playbook playbooks/provision.yml
 
 inventory/ec2.py --refresh-cache > /dev/null
-ansible-playbook playbooks/deploy.yml
+ansible-playbook playbooks/install-application.yml
+ansible-playbook playbooks/decomission.yml
+
